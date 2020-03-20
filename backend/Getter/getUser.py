85ce1b54d9833bd79fe6719c79d6e2c45d291365
vmai <vmai@inovex.de>
2020-03-20 23:54:18 +0100

    done

---
+++ backend/Getter/getUser.py
@@ -0,0 +1,20 @@
+import pandas
+
+
+def get_user_by_username(username):
+    user_data = pandas.read_csv('Registration/user_data.csv')
+
+    if user_data.empty:
+        return 0
+
+    for index, row in user_data.iterrows():
+        if row['username'] == username:
+            return {
+                "success": "true",
+                "username:": row['username'],
+                "email": row['email'],
+                "state": row['state']
+            }
+    return {
+        "success": "false"
+    }
--- backend/Login/login.py
+++ backend/Login/login.py
@@ -2,13 +2,12 @@ import pandas
 
 
 def check_user(username, password):
-    user_data = pandas.read_csv('Login/login_credentials.csv', names=['user', 'password'])
+    user_data = pandas.read_csv('Registration/user_data.csv')
 
     if user_data.empty:
         return 0
 
     for index, row in user_data.iterrows():
-        if row['user'] == username and row['password'] == password:
+        if row['username'] == username and row['password'] == password:
             return 1
-
     return 0
