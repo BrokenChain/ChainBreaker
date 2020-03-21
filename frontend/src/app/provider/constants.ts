53c1897207bff413b0508023de7d27b525b072dc
neihtfool <q.thien.nguyen@outlook.de>
2020-03-21 20:08:17 +0100

    update baseurl

--- frontend/src/app/provider/constants.ts
+++ frontend/src/app/provider/constants.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://71ed697c.ngrok.io/';
+export const BASE_URL = 'https://bf4b70bd.ngrok.io/';
 export const LOGIN = 'login';
 export const REGISTER = 'register';
 export const TABLE = 'table';
